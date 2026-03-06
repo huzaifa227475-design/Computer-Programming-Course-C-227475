b264aae502ee79a8d2a83f9828bfa401805917f8
huzaifa227475-design <huzaifa227475@gmail.com>
2026-03-06 07:06:16 -0800

    Add files via upload

---
+++ Lab Number Second/Lab Task/Lab  Second ( TASK (_5) ).cpp
@@ -0,0 +1,24 @@
+// Name: Muhammad Huzaifa
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0742
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // printing name 10 times
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa " << endl;
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa" << endl;
+    cout << "Muhammad Huzaifa" << endl;
+
+    return 0;  // program ends
+}

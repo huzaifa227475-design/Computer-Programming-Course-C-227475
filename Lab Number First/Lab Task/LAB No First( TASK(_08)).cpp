015ca425575df83f5dff42632eb019e59033a8cc
huzaifa227475-design <huzaifa227475@gmail.com>
2026-03-06 06:50:40 -0800

    Add files via upload

---
+++ Lab Number First/Lab Task/LAB No First( TASK(_08)).cpp
@@ -0,0 +1,16 @@
+// Name: Muhammad Huzaifa
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0742
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float aB = 10.2;  // Declaring float variable
+
+    cout << aB;  // Printing value of aB
+
+    return 0;  // Program ends successfully
+}
